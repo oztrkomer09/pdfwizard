fa78cfebb0d28ebf2c1dbf0672a7655d78db5b0f
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-12 13:29:43 +0300

    feat: create footer

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./shared/Navbar";
+import Footer from "./shared/Footer";
 
 const Layout = ({ children }) => {
   return (
     <div className="wrapper-layout">
       <Navbar />
       <main>{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
---
+++ frontend/src/components/shared/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../../assets/images/wizard_hat.png";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section">
+        <div className="footer-left">
+          <div className="logo-brand">
+            <img src={logo} alt="logo" />
+            <p>pdfwizard</p>
+          </div>
+          <p>Lets do magic!</p>
+        </div>
+        <div className="footer-right">
+          <div className="footer-about">
+            <h4>About Us</h4>
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+            <p>Contact</p>
+          </div>
+          <div className="footer-partners">
+            <h4>Partners</h4>
+            <p>jotform.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
