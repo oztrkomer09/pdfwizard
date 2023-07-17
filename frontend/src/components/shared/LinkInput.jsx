37698310ef7c393175c163e5aef6b18768d3a78d
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-17 11:39:38 +0300

    style: refactor hero input section

--- frontend/src/components/shared/LinkInput.jsx
+++ frontend/src/components/shared/LinkInput.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import InputButtonComponent from "./InputButtonComponent";
+import human1 from "../../assets/images/human1.png";
+import frame from "../../assets/images/empty_frame.png";
+import linkedIn from "../../assets/images/linkedIn.png";
 
 const LinkInput = () => {
   return (
-    <div className="section-wrapper">
-      <div className="link-input-section">
-        <p>Create free PDFs with pdfwizard</p>
-        <InputButtonComponent />
+    <div className="link-section-wrapper">
+      <div className="link-input-hero-section">
+        <div className="link-input-left-section">
+          <h2>We Help You To Get The Perfect CV</h2>
+          <p>
+            Copy the link of your profile, paste it in the URL part in the
+            cvwizard and run. Finally, you can show your CV.
+          </p>
+          <InputButtonComponent />
+        </div>
+        <div className="link-input-right-section">
+          <img src={frame} alt="frame" className="frame" />
+          <img src={human1} alt="human1" className="human1" />
+        </div>
+      </div>
+      <div className="link-section-bottom-bar">
+        <p>We're Supporting</p>
+        <img src={linkedIn} alt="linkedIn" />
       </div>
     </div>
   );
