21b2e6b97e11fb3a17c1d1b70d279dea3ab554dc
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-19 16:28:00 +0300

    style: refactor some sections

--- server/index.js
+++ server/index.js
@@ -7,8 +7,16 @@ const port = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
+  next();
+})
+
 app.post('/scrape', async (req, res) => {
   const { url } = req.body;
+  console.log(req.body);
 
   try {
     const data = await runLinkedInScraper(url);
