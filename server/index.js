5944448d86d786b136c625aa52577b2f5ac53166
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-20 14:25:11 +0300

    delete cors package

--- server/index.js
+++ server/index.js
@@ -1,27 +1,16 @@
 import express from 'express';
 import runLinkedInScraper from './scraper.js';
 import fs from 'fs';
-import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
-
-
 app.use(express.json());
 
-app.use(cors())
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-//   res.setHeader('Access-Control-Allow-Methods', 'Content-Type', 'Authorization');
-//   next();
-// })
 
 app.post('/scrape', async (req, res) => {
   const { url } = req.body;
-  console.log(req.body);
 
   try {
     const data = await runLinkedInScraper(url);
