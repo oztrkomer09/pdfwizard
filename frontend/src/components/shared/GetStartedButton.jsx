b11b3b3c97dd3a2d675b9756446d837f966ed923
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-24 09:55:13 +0300

    feat: add scroll to top function to buttons

--- frontend/src/components/shared/GetStartedButton.jsx
+++ frontend/src/components/shared/GetStartedButton.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const GetStartedButton = () => {
-  return <button className="get-started">Get Started Now!</button>;
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  return (
+    <button onClick={scrollTop} className="get-started">
+      Get Started Now!
+    </button>
+  );
 };
 
 export default GetStartedButton;
--- frontend/src/components/shared/GetStartedButtonArrow.jsx
+++ frontend/src/components/shared/GetStartedButtonArrow.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import arrow from "../../assets/images/right_arrow.png";
 
 const GetStartedButtonArrow = () => {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
   return (
-    <button className="get-started-arrowed">
+    <button onClick={scrollTop} className="get-started-arrowed">
       Get Started Now!
       <img src={arrow} alt="arrow" className="button-arrow" />
     </button>
