434b1e5cfde5798bc97a1028c2d05ffc357de9d1
omer.ozturk <ofozturk@eskisehir.edu.tr>
2023-07-27 10:57:28 +0300

    feat: create modal to choose cv template

---
+++ frontend/src/components/shared/Modal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import template1 from "../../assets/images/template1.png";
+import template2 from "../../assets/images/template2.png";
+
+const Modal = ({ setIsModalOpen }) => {
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span onClick={closeModal} className="close">
+          &times;
+        </span>
+        <p className="modal-title">Choose Template</p>
+        <p className="modal-info">
+          Explore the most suitable templates to create CV in minutes
+        </p>
+        <div className="modal-templates">
+          <div className="modal-template">
+            <img src={template1} alt="template1" />
+            <p className="template-title">Classical</p>
+          </div>
+          <div className="modal-template">
+            <img src={template2} alt="template2" />
+            <p className="template-title">Official</p>
+          </div>
+          <div className="modal-template">
+            <img src={template1} alt="template1" />
+            <p className="template-title">Creative</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
